98319eb61aabd012f7d76654d7ee8590761da10e
Shahmeer Ali <160905960+ShahmeerAli2002@users.noreply.github.com>
2024-03-06 15:20:34 +0500

    Add files via upload

---
+++ 3c.ts
@@ -0,0 +1,190 @@
+// const num1 = 1;
+// const num2 = 2;
+// console.log(num1+num2);
+
+// const num1 = 23
+// const num2 = 23
+
+// const sum = num1 + num2 + 10
+//  console.log(sum)
+
+
+//  const num3 = 23
+
+//  const num4 = 24 
+
+//  const sub = num3 - num4 
+ 
+//  console.log()
+
+//    const num1 = 21 
+   
+//    const num2 = 5 
+
+//     const mod = num1 % num2 
+
+//     console.log(mod)
+
+// const num1 =5
+// const num2 =2
+// const power = num1 ** num2
+// console.log(power)
+
+
+// const firstName= "shahmir"
+//  const lastName= 2 
+
+//   console.log(firstName+" "+lastName)
+
+// let n =5 
+//  n = n +5 
+
+//  console.log(n)
+//  console.log (10== 10); 
+//  console.log(10===10);
+
+
+//   console.log (10== '10'); 
+//   console.log(10==='10');
+
+//  console.log(10 !=5);
+//   console .log(5>4);
+//   console .log(2<=5)
+
+
+// let numb  =  23
+// let numb1  = 13
+// let sum = numb + numb1
+
+// console.log(sum)
+
+
+// let  numb = 23 
+// let numb1 = 23
+//  numb  = numb + 45
+// let sum = numb+ numb1
+
+// console.log(sum)
+
+// let  num1 : number = 10 * 13 ;
+
+// console.log(num1)
+ 
+// let  num1 = 10 *20 ;
+// let  num2 =  20 / 20;
+// let md = num1 + num1;
+
+// console.log(md)
+
+
+// let num1 = 10 / 20 ;
+
+// console.log(num1);
+
+
+// let num1 = 20
+// let num2 = 17
+
+// let sum = num1 - num2
+
+// console.log(sum);
+
+// let num1 = 20
+// let num2 = 30
+
+// let n1n2 = num1 == num2
+
+// console.log(n1n2)
+
+// let num1 = 20
+// let num2 = 20
+
+// let n1n2 = num1 == num2
+
+// console.log(n1n2)
+
+
+// let num1 = 20
+// let num2 = 20
+//   num2 = 30
+// let n1n2 = num1 == num2
+
+// console.log(n1n2)
+ 
+
+// let num1 = 14
+// let num2 = 13
+
+// let n1n2 = num1 > num2
+
+// console.log(n1n2)
+
+// let num1 = 13
+// let num2 = 30
+
+// let n1n2 = num1 < num2
+
+// console.log(n1n2)
+ 
+// let num1 = 39
+// let num2 = 30
+
+// let n1n2 = num1 >= num2
+
+// console.log(n1n2)
+
+// let num1 = 20
+// let num2 = 30
+
+// let n1n2 = num1 ** num2
+
+// console.log(n1n2)
+
+// let mod  = 3 
+
+// let mod1 = 2
+
+// let mod2 = mod % mod1  + 4 ;
+
+//  console.log(mod2);
+
+ 
+// use operator in typing 
+
+// let names = 'daniyal ';
+
+// let name1 = 'Taha';
+
+// console.log( names + name1);
+
+// let names = 'daniyal ';
+
+// let name1 = 'Taha';
+   
+//  names = "Ali"
+//  name1 =  "john"
+
+// console.log( names+"   "+ name1);
+
+
+// let check :string= "Shahmeer"
+// let check1 : string ='Akbar'
+ 
+// let check3 : string = check + "   "  +check1  + "  " +"uzair";
+
+// console.log(check3);
+
+let you  : string =  " anas"
+
+
+
+
+
+
+
+
+
+
+
+
+
